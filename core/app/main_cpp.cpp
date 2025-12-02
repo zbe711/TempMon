3f12cfe892f8fc144c4b937be3dccf2fc799dec6
Zebbiche Badr Eddine <Badr.Eddine@gmx.de>
2025-12-02 10:37:23 +0100

    Add HelloWorld entry points for C99 and C++ in app directory

---
+++ core/app/main_cpp.cpp
@@ -0,0 +1,15 @@
+// Simple C++ HelloWorld entry point
+#include <iostream>
+
+static void hello_cpp()
+{
+    std::cout << "Hello, world from C++!" << std::endl;
+}
+
+int main()
+{
+    hello_cpp();
+    return 0;
+}
+
+
