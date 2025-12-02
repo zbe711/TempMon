5489935fc30b7d0a5f7b1b92be30abfec82b3bb6
Zebbiche Badr Eddine <Badr.Eddine@gmx.de>
2025-12-02 14:20:12 +0100

    Add Sphinx configuration and requirements for documentation setup

---
+++ docs/conf.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from datetime import datetime
+
+project = "Temperature Monitoring Requirements"
+author = "Zebbiche Badr Eddine"
+year = datetime.now().year
+copyright = f"{year}, {author}"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_needs",
+    "sphinx_rtd_theme",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "sphinx_rtd_theme"
+
+needs_types = [
+    {
+        "directive": "req",
+        "title": "Requirement",
+        "prefix": "REQ_",
+        "color": "#BFD8D2",
+        "style": "node",
+    },
+]
