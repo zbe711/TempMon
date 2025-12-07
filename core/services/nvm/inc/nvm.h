fd487c0ede521d52d11c3a624a9a3a54dfd3f3c5
Zebbiche Badr Eddine <Badr.Eddine@gmx.de>
2025-12-07 20:27:30 +0100

    Add NVM header and source files with init and read functionality

--- core/services/nvm/inc/nvm.h
+++ core/services/nvm/inc/nvm.h
@@ -1,2 +1,12 @@
-/* NVM service - Non-Volatile Memory interface for EEPROM access */
+#ifndef NVM_H
+#define NVM_H
 
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include "nvm_config.h"
+
+void nvm_init(void);
+bool nvm_read(nvm_block_id_t block_id, void* block, size_t size);
+
+#endif
--- core/services/nvm/src/nvm.c
+++ core/services/nvm/src/nvm.c
@@ -1,2 +1,32 @@
-/* NVM service implementation */
+#include "nvm.h"
+#include "nvm_config.h"
+#include "i2c.h"
+#include <stdbool.h>
 
+#define NVM_EEPROM_I2C_ADDRESS  0x50
+
+static const uint16_t g_block_address_map[NVM_BLOCK_COUNT] =
+{
+    [NVM_BLOCK_HW_CONFIG] = EEPROM_HW_CONFIG_ADDRESS
+};
+
+static void* g_i2c_handle = NULL;
+
+void nvm_init(void)
+{
+    i2c_init();
+    g_i2c_handle = i2c_open(NVM_I2C_BUS);
+}
+
+bool nvm_read(nvm_block_id_t block_id, void* block, size_t size)
+{
+    bool result = false;
+    
+    if (block != NULL && g_i2c_handle != NULL && block_id < NVM_BLOCK_COUNT)
+    {
+        uint16_t reg_addr = g_block_address_map[block_id];
+        result = i2c_read(g_i2c_handle, NVM_EEPROM_I2C_ADDRESS, reg_addr, (uint8_t*)block, size);
+    }
+    
+    return result;
+}
